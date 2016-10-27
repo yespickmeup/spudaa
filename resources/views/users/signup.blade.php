77f2e896d07defc2a4afa473db36beea33ea0864
synsoftech <synsoftech@synsoftech-H61M-DS2>
2016-10-27 14:29:35 +0800

    header

--- resources/views/users/signup.blade.php
+++ resources/views/users/signup.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Alumni Association | Signup</title>
+          <link rel="shortcut icon" type="image/png" href="../src/images/system/spud-alumni-logo.png" />
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <link rel="stylesheet" href="{{ URL::to('src/bootstrap/css/bootstrap.min.css') }}"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
